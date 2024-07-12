e50dae941ada1461777bb2e4d4ed6cbf20f4dce5
KAPAKRO <KAPAKTUYUL03@gmail.com>
2024-07-12 23:59:26 +0700

    updated

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -6,6 +6,10 @@ import { z } from "zod";
 import { Button } from "@/components/ui/button";
 import { Form } from "@/components/ui/form";
 import CustomFormField from "../CustomFormField";
+import SubmitButton from "../SubmitButton";
+import { useState } from "react";
+import { UserFormValidation } from "@/lib/validation";
+import { useRouter } from "next/navigation";
 
 
 export enum FormFieldType {
@@ -18,23 +22,31 @@ export enum FormFieldType {
   SKELETON = "skeleton",
 }
 
-
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-});
-
 const PatientForm = () => {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const form = useForm<z.infer<typeof UserFormValidation>>({
+    resolver: zodResolver(UserFormValidation),
     defaultValues: {
-      username: "",
+      name: "",
+      email: "",
+      phone:"",
     },
   });
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values);
+  async function onSubmit({ name, email, phone }: z.infer<typeof UserFormValidation>) {
+    setIsLoading(true);
+
+    try {
+      // const userData = { name, email, phone };
+
+      // const user = await createUser(userData);
+
+      // if(user) router.push(`/patients/${user.id}/register`);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
@@ -55,7 +67,25 @@ const PatientForm = () => {
           iconAlt="User"
         />
 
-        <Button type="submit">Submit</Button>
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={form.control}
+          name="email"
+          label="Email"
+          placeholder="Masukkan Email anda"
+          iconSrc="/assets/icons/email.svg"
+          iconAlt="email"
+        />
+
+        <CustomFormField
+          fieldType={FormFieldType.PHONE_INPUT}
+          control={form.control}
+          name="phone"
+          label="Phone Number"
+          placeholder="+62 812-3456-7890"
+        />
+
+        <SubmitButton isLoading={isLoading}> Get Started </SubmitButton>
       </form>
     </Form>
   );
